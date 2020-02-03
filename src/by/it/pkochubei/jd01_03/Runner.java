0d3d8ece975cd7cf96872a9072221a550d282c04
Pavel <pashakochybey@mail.ru>
2020-02-03 17:32:34 +0200

    jd01_06 TaskA

--- src/by/it/pkochubei/jd01_03/Runner.java
+++ src/by/it/pkochubei/jd01_03/Runner.java
@@ -8,6 +8,8 @@ public class Runner {
         double[] array = InOut.getArray(str);
         InOut.printArray(array);
         InOut.printArray(array,"M",4);
-        //
+        double min=Helper.findMin(array);
+        double max = Helper.findMax(array);
+        Helper.sort(array);
     }
 }
